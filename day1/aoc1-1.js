7a660f90ec38d70cc72e8a91800e972252af1533
piotrbartnik <piotr.bartnik@hotmail.com>
2019-01-02 23:06:16 +0100

    1-1 finished

--- day1/aoc1-1.js
+++ day1/aoc1-1.js
@@ -1,2 +1,10 @@
 let input = document.getElementsByTagName('pre');
 let inputArr = input[0].innerText.split('\n');
+let zero = 0;
+for (i=0;i<inputArr.length;i++){
+  inputArr[i] = parseInt(inputArr[i]);
+}
+for (i=0;i<inputArr.length-1;i++) {
+  zero += inputArr[i]
+}
+console.log(zero);

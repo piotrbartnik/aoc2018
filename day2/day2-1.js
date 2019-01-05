b80b2c9915bf2ca179182424d3959b76501d2d36
piotrbartnik <piotr.bartnik@hotmail.com>
2019-01-05 22:06:24 +0100

    added function

--- day2/day2-1.js
+++ day2/day2-1.js
@@ -1,3 +1,4 @@
 let input = document.getElementsByTagName('pre');
 let inputArr = input[0].innerText.trim().split('\n');
-inputArr = inputArr.map(x=>x.split('').sort().join(''))
+inputArr = inputArr.map(x=>x.split('').sort().join(''));
+

a055dc9c5179de77085f0a6273c7963f57e751b0
piotrbartnik <piotr.bartnik@hotmail.com>
2019-01-22 21:54:33 +0100

    day 2-1

--- day2/day2-1.js
+++ day2/day2-1.js
@@ -1,3 +1,20 @@
 let input = document.getElementsByTagName('pre');
 let inputArr = input[0].innerText.trim().split('\n');
 inputArr = inputArr.map(x=>x.split('').sort().join(''))
+let result= [];
+result[0] = boxIDs.reduce((acc, val) => {
+  let letters = val.split('').reduce((acc, val) => {
+      acc[val] = (acc[val] || 0) + 1;
+      return acc;
+  }, {});
+
+  let has2 = false, has3 = false;
+  for (let a of Object.values(letters)) {
+      if (a === 2) has2 = true;
+      if (a === 3) has3 = true;
+  }
+  if (has2) ++acc[0];
+  if (has3) ++acc[1];
+
+  return acc;
+}, [ 0, 0 ]).reduce((acc, val) => acc * val, 1);

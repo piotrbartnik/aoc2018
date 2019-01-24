cb47e750307ec381c58171c60a605101f93b5500
piotrbartnik <piotr.bartnik@hotmail.com>
2019-01-24 21:45:06 +0100

    added day 2

--- day2/day2-2.js
+++ day2/day2-2.js
@@ -1,3 +1,17 @@
-let input = document.getElementsByTagName('pre');
-let inputArr = input[0].innerText.trim().split('\n');
-inputArr = inputArr.map(x=>x.split('').sort().join(''));
+for (let item of boxIDs) {
+    let lettersA = item.split('');
+    for (let i = boxIDs.indexOf(item) + 1; i < boxIDs.length; ++i) {
+        let diff = [];
+        let lettersB = boxIDs[i].split('');
+        for (let l = 0; l < lettersA.length; ++l) {
+            if (lettersA[l] !== lettersB[l]) {
+                if (diff.push(lettersA[l]) >= 2)
+                    break;
+            }
+        }
+        if (diff.length === 1) {
+            result[1] = item.replace(diff[0], '');
+            break;
+        }
+    }
+}

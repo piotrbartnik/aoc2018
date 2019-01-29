e13be66ffc16ce66ff77bfd3fd67f59692720a2e
piotrbartnik <piotr.bartnik@hotmail.com>
2019-01-29 22:30:42 +0100

    initial

--- day2/day2-2.js
+++ day2/day2-2.js
@@ -1,3 +1,6 @@
+let input = document.getElementsByTagName('pre');
+let boxIDs = input[0].innerText.trim().split('\n');
+let result= [];
 for (let item of boxIDs) {
     let lettersA = item.split('');
     for (let i = boxIDs.indexOf(item) + 1; i < boxIDs.length; ++i) {
@@ -13,5 +16,6 @@ for (let item of boxIDs) {
             result[1] = item.replace(diff[0], '');
             break;
         }
-    }
+    }    
 }
+console.log(result[1]);
